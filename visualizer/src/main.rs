7487cf673e3302e248428fdf366ac98c017f4b1c
Paulo Bressan <paulo.bressan@outlook.com>
2024-04-23 15:21:56 -0300

    Static pot asset example (#50)

--- visualizer/src/main.rs
+++ visualizer/src/main.rs
@@ -7,32 +7,19 @@ use tracing::Level;
 mod api;
 mod asteroid;
 mod map;
+mod pot;
 mod ships;
 
 #[derive(Component)]
 struct MyCameraMarker;
 
-fn setup_camera(mut commands: Commands) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default()).insert(PanCam {
         min_scale: 1.,
         max_scale: Some(3.),
         ..default()
     });
 }
-
-fn setup_ui(mut commands: Commands) {
-    commands.spawn(NodeBundle {
-        background_color: Color::RED.into(),
-        ..Default::default()
-    });
-}
-
-// fn setup_environment(mut commands: Commands, asset_server: Res<AssetServer>) {
-//     commands.spawn(SpriteBundle {
-//         texture: asset_server.load("background.gif"),
-//         ..default()
-//     });
-// }
 
 fn main() {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
@@ -42,9 +29,12 @@ fn main() {
         .add_plugins(PanCamPlugin)
         .add_plugins(EntropyPlugin::<WyRand>::default())
         .add_plugins(DefaultPickingPlugins)
-        .add_systems(Startup, setup_camera)
-        .add_systems(Startup, setup_ui)
-        // .add_systems(Startup, setup_environment)
-        .add_plugins((map::MapPlugin, ships::ShipsPlugin, asteroid::AsteroidPlugin))
+        .add_systems(Startup, setup)
+        .add_plugins((
+            map::MapPlugin,
+            ships::ShipsPlugin,
+            asteroid::AsteroidPlugin,
+            pot::PotPlugin,
+        ))
         .run();
 }
