5f3b30a07ae67ea1dfb73ef22a1490ff35aef673
Francisco Joray <franjoray@gmail.com>
2024-05-03 17:50:23 -0300

    added pellets tx indexing.

--- offchain/tests/user/gather-fuel.ts
+++ offchain/tests/user/gather-fuel.ts
@@ -4,15 +4,17 @@ import { printTxURL } from "../../utils.ts";
 
 const gather_amount = 20n;
 const ship_tx_hash =
-  "6ca378639a51b1283ec2c84deb772f62bff59603eca230aeb711fedee8389d69";
+  "f1079755f32839c7c55796335e6835a0aafa8ce9b3e15fed287e0d971826881c";
 const pellet_tx_hash =
-  "b1d5dedc6c9ba333d8eab4c9f05edda4563fc313fc9b124393ec3b64e3676fb0";
+  "f7f8f8298876cabb81357c1d7e89e2e01bf8956a566063f12ceee7b6901d0334";
+const pellet_tx_index = 1;
 
 const txHash = await gatherFuel(
   admin_token,
   gather_amount,
   ship_tx_hash,
-  pellet_tx_hash
+  pellet_tx_hash,
+  pellet_tx_index
 );
 
 printTxURL(txHash);
