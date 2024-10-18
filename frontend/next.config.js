3c5b711fb5c20d734d49e43f1ebb56e4119111d3
EehMauro <maurojoaquinescudero@gmail.com>
2024-10-18 15:04:44 -0300

    Bulk commit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,13 +4,6 @@ const withMDX = require('@next/mdx')()
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   transpilePackages: ['next-mdx-remote'],
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      type: 'asset/source',
-    });
-    return config;
-  },
 }
  
 module.exports = withMDX(nextConfig)
