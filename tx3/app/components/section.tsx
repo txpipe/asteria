0998a078ba0328f729e374c27498e3ab4c5dc570
Maximiliano Duthey <maxi.duthey@txpipe.io>
2025-07-04 13:09:18 -0300

    fix: update section component width to use responsive units

--- tx3/app/components/section.tsx
+++ tx3/app/components/section.tsx
@@ -9,7 +9,7 @@ interface SectionProps {
 export function Section({ children, title, className }: PropsWithChildren<SectionProps>) {
   return (
     <section className={clsx('h-dvh overflow-y-hidden overflow-x-hidden snap-start flex', className)}>
-      <div className="flex flex-col gap-6 items-center mx-15 my-16 w-150 overflow-hidden bg-[#0A0A0A]/80 rounded-[20px] border p-6 border-primary-50">
+      <div className="flex flex-col gap-6 items-center mx-15 my-16 w-[50dvw] overflow-hidden bg-[#0A0A0A]/80 rounded-[20px] border p-6 border-primary-50">
         {title && <h2 className="text-xl text-primary-50">{title}</h2>}
         {children}
       </div>
